c2fccc955a9ccdb06ea3e7a43250b24e7ca9a562
Josh Pulley <josh.pulley@icloud.com>
2021-11-12 15:31:52 -0600

    updated register

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -8,6 +8,7 @@ export const Register = () => {
     const username = useRef()
     const bio = useRef()
     const password = useRef()
+    const email = useRef()
     const verifyPassword = useRef()
     const passwordDialog = useRef()
     const history = useHistory()
@@ -21,7 +22,8 @@ export const Register = () => {
                 "first_name": firstName.current.value,
                 "last_name": lastName.current.value,
                 "bio": bio.current.value,
-                "password": password.current.value
+                "password": password.current.value,
+                "email": email.current.value
             }
 
             return fetch("http://127.0.0.1:8000/register", {
@@ -67,6 +69,10 @@ export const Register = () => {
                     <input ref={username} type="text" name="username" className="form-control" placeholder="Username" required />
                 </fieldset>
                 <fieldset>
+                    <label htmlFor="email">Email</label>
+                    <input ref={email} type="text" name="email" className="form-control" placeholder="Username" required />
+                </fieldset>
+                <fieldset>
                     <label htmlFor="inputPassword"> Password </label>
                     <input ref={password} type="password" name="password" className="form-control" placeholder="Password" required />
                 </fieldset>
@@ -75,7 +81,7 @@ export const Register = () => {
                     <input ref={verifyPassword} type="password" name="verifyPassword" className="form-control" placeholder="Verify password" required />
                 </fieldset>
                 <fieldset>
-                    <label htmlFor="verifyPassword"> Verify Password </label>
+                    <label htmlFor="bio"> Bio </label>
                     <textarea ref={bio} name="bio" className="form-control" placeholder="Let other gamers know a little bit about you..." />
                 </fieldset>
                 <fieldset style={{
